7207a20a78b9cace5d8a962168ba6f18c00b74da
prog-coder24 <vaishnavisawant1224@gmail.com>
2021-04-16 20:54:10 +0530

    first commit

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Expense Item!</h2>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
